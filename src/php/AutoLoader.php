461a58981065de7cc99f22e857928fe591de8f0a
Devwarlt <devwarlt@loesoft.org>
2020-10-28 01:03:20 -0200

    Create AutoLoader.php

---
+++ src/php/AutoLoader.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: devwarlt
+ * Date: 28/10/2020
+ * Time: 01:00
+ */
+
+namespace php;
+
+
+final class AutoLoader
+{
+    public static function register()
+    {
+        spl_autoload_register(function ($class) {
+            $file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+
+            if (!file_exists($file))
+                return false;
+
+            require($file);
+            return true;
+        });
+    }
+}
