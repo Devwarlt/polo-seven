000e3ec052cd27db64c304d191d81ac2d6fbe27f
Devwarlt <devwarlt@loesoft.org>
2020-10-28 02:31:45 -0200

    Update ProdutoDAO.php

--- src/php/dao/ProdutoDAO.php
+++ src/php/dao/ProdutoDAO.php
@@ -8,27 +8,71 @@
 
 namespace php\dao;
 
+use php\dao\db\MySQLDatabase as mysqldb;
+use php\dao\db\SQLQuery as sqlquery;
 use php\model\ProdutoModel;
 
 final class ProdutoDAO
 {
     public function criarProduto(ProdutoModel $produto): bool
     {
-        // TODO: Implement criarProduto() method.
+        $mysql = mysqldb::getSingleton();
+        return $mysql->insert(
+            new sqlquery(
+                "INSERT INTO `produtos`(`nome`, `preco_unitario`, `total_unidades`) VALUES (':nome', :preco_unitario, :total_unidades)",
+                [
+                    ":nome" => $produto->getNome(),
+                    ":preco_unitario" => $produto->getPrecoUnitario(),
+                    ":total_unidades" => $produto->getTotalUnidades()
+                ]
+            ));
     }
 
     public function consultarProduto($id): ProdutoModel
     {
-        // TODO: Implement consultarProduto() method.
+        $mysql = mysqldb::getSingleton();
+        $result = $mysql->select(
+            new sqlquery(
+                "SELECT * FROM `produtos` WHERE `id` = :id",
+                [":id" => $id]
+            )
+        );
+        if ($result === null)
+            return null;
+
+        $produto = new ProdutoModel(
+            $result->id,
+            $result->nome,
+            $result->preco_unitario,
+            $result->total_unidades
+        );
+        return $produto;
     }
 
     public function alterarProduto(ProdutoModel $produto): bool
     {
-        // TODO: Implement alterarProduto() method.
+        $mysql = mysqldb::getSingleton();
+        return $mysql->update(
+            new sqlquery(
+                "UPDATE `produtos` SET `id` = :id, `nome` = ':nome', `preco_unitario` = :preco_unitario, `total_unidades` = :total_unidades",
+                [
+                    ":id" => $produto->getId(),
+                    ":nome" => $produto->getNome(),
+                    ":preco_unitario" => $produto->getPrecoUnitario(),
+                    ":total_unidades" => $produto->getTotalUnidades()
+                ]
+            )
+        );
     }
 
     public function removerProduto($id): bool
     {
-        // TODO: Implement removerProduto() method.
+        $mysql = mysqldb::getSingleton();
+        return $mysql->delete(
+            new sqlquery(
+                "DELETE FROM `produtos` WHERE `id` = :id",
+                [":id" => $id]
+            )
+        );
     }
 }
