4b464bdc451af1f139d0ee33fee1d263ee10ce63
Devwarlt <devwarlt@loesoft.org>
2020-10-28 01:56:00 -0200

    Update SQLQuery.php

--- src/php/dao/db/SQLQuery.php
+++ src/php/dao/db/SQLQuery.php
@@ -10,5 +10,32 @@ namespace php\dao\db;
 
 final class SQLQuery
 {
+    private $sql, $params;
 
+    public function __construct($sql, array $params = null)
+    {
+        $this->sql = $sql;
+        $this->params = $params;
+    }
+
+    public function getQuery(): string
+    {
+        if ($this->params !== null)
+            $this->sql = self::replaceArray($this->sql, $this->params);
+
+        return $this->sql;
+    }
+
+    private static function replaceArray($value, array $array): string
+    {
+        foreach ($array as $innerKey => $innerValue)
+            $value = self::replace($innerKey, $innerValue, $value);
+
+        return $value;
+    }
+
+    private static function replace($key, $value, $subject): string
+    {
+        return str_replace($key, $value, $subject);
+    }
 }
