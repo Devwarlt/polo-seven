5e2d9f3671bfeeb91f88d9eca15e4803faafaaf9
Devwarlt <devwarlt@loesoft.org>
2020-10-28 18:27:27 -0200

    Create dashboard.php

---
+++ src/dashboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Polo Seven - Dashboard</title>
+</head>
+<body>
+<h1>Polo Seven - Dashboard</h1>
+<hr/>
+</body>
+</html>
