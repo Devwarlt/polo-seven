309be9be12401181856d48ee625d05c21f431057
Devwarlt <devwarlt@loesoft.org>
2020-10-28 01:46:23 -0200

    Added Pagamento missing DAO + Model refs

---
+++ src/php/dao/PagementoDAO.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: devwarlt
+ * Date: 28/10/2020
+ * Time: 01:44
+ */
+
+namespace php\dao;
+
+use php\model\PagamentoModel;
+
+final class PagementoDAO
+{
+    public function criarPagemento(PagamentoModel $pagamento)
+    {
+        // TODO: Implement criarPagemento() method.
+    }
+
+    public function consultarPagemento($id)
+    {
+        // TODO: Implement consultarPagemento() method.
+    }
+
+    public function alterarPagemento(PagamentoModel $pagamento)
+    {
+        // TODO: Implement alterarPagemento() method.
+    }
+
+    public function removerPagemento($id)
+    {
+        // TODO: Implement removerPagemento() method.
+    }
+}
---
+++ src/php/model/PagamentoModel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: devwarlt
+ * Date: 28/10/2020
+ * Time: 01:44
+ */
+
+namespace php\model;
+
+final class PagamentoModel
+{
+
+}
