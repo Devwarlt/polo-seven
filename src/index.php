64a6cae9a73a6afb5e976e85d580e3fabb865db1
Devwarlt <devwarlt@loesoft.org>
2020-10-29 19:07:49 -0200

    Apply basic design for main index page

--- src/index.php
+++ src/index.php
@@ -1,77 +1,84 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="pt" style="height: 100%;">
 <head>
     <meta charset="UTF-8">
     <title>Polo Seven - Início</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
 </head>
-<body>
-<h1>Polo Seven - Início</h1>
-<?php
+<body style="height: 100%;" class="h-100 row align-items-center  alert-light">
+<div class="col card-body">
+    <form class="card card-header form-inline alert-secondary border-secondary" action="/php/MVCRouter.php"
+          method="post">
+        <input type="hidden" name="controller" value="login"/>
+        <h3 class="card-header">Login</h3>
+        <div class="card-body card-group">
+            <input type="text" id="nome" name="nome" placeholder="Nome"/>
+            &nbsp;
+            <input type="password" id="senha" name="senha" placeholder="Senha"/>
+            &nbsp;
+            <input class="btn btn-sm btn-outline-primary" type="submit" value="Entrar"/>&nbsp;
+            <input class="btn btn-sm btn-outline-secondary" type="reset" value="Resetar"/>
+        </div>
+    </form>
+    <?php
 
-include "php/controller/LoginController.php";
+    include "php/controller/LoginController.php";
 
-use php\controller\LoginController as loginctrl;
-use php\PhpUtils as phputils;
+    use php\controller\LoginController as loginctrl;
+    use php\PhpUtils as phputils;
 
-session_start();
+    session_start();
 
-$login = loginctrl::getSingleton();
-
-if ($login->verificarSessaoLogin()) {
-    header("Location:/dashboard.php");
-    return;
-}
-
-if (isset($_GET["err"])) {
-    include "php/PhpUtils.php";
-
-    $utils = phputils::getSingleton();
-    $err = urldecode($_GET["err"]);
-    if ($utils->checkPhpInjection($err)) {
-        $utils->onRawIndexErr("Php Injection detectado!", "/index.php");
+    $login = loginctrl::getSingleton();
+    if ($login->verificarSessaoLogin()) {
+        header("Location:/dashboard.php");
         return;
     }
 
-    echo "
+    if (isset($_GET["err"])) {
+        include "php/PhpUtils.php";
+
+        $utils = phputils::getSingleton();
+        $err = urldecode($_GET["err"]);
+        if ($utils->checkPhpInjection($err)) {
+            $utils->onRawIndexErr("Php Injection detectado!", "/index.php");
+            return;
+        }
+
+        echo "
     <hr/>
-    <h3 style='color: darkred'>Erro!</h3>
-    <p style='color: red' align='justify'>$err</p>
+    <div class='alert alert-danger border-danger alert-dismissible fade show' role='alert'>
+        <h3 style='color: darkred'>Erro!</h3>
+        <hr/>
+        <p style='color: red' align='justify'>$err</p>
+        <button type='button' class='close' data-dismiss='alert' aria-label='Fechar'>&times;</button>
+    </div>
     ";
-}
-
-
-if (isset($_GET["ok"])) {
-    include "php/PhpUtils.php";
-
-    $utils = phputils::getSingleton();
-    $ok = urldecode($_GET["ok"]);
-    if ($utils->checkPhpInjection($ok)) {
-        $utils->onRawIndexErr("Php Injection detectado!", "/index.php");
-        return;
     }
 
-    echo "
+    if (isset($_GET["ok"])) {
+        include "php/PhpUtils.php";
+
+        $utils = phputils::getSingleton();
+        $ok = urldecode($_GET["ok"]);
+        if ($utils->checkPhpInjection($ok)) {
+            $utils->onRawIndexErr("Php Injection detectado!", "/index.php");
+            return;
+        }
+
+        echo "
     <hr/>
-    <h3 style='color: green'>Sucesso!</h3>
-    <p style='color: limegreen' align='justify'>$ok</p>
+    <div class='alert alert-success border-success alert-dismissible fade show' role='alert'>
+        <h3 style='color: green'>Sucesso!</h3>
+        <hr/>
+        <p style='color: limegreen' align='justify'>$ok</p>
+        <button type='button' class='close' data-dismiss='alert' aria-label='Fechar'>&times;</button>
+    </div>
     ";
-}
-?>
-<hr/>
-<form action="/php/MVCRouter.php" method="post">
-    <input type="hidden" name="controller" value="login"/>
-    <h2>Login</h2>
-    <br/>
-    <label for="nome">Nome:</label>
-    <br/>
-    <input type="text" id="nome" name="nome" placeholder="Nome"/>
-    <br/>
-    <label for="senha">Senha:</label>
-    <br/>
-    <input type="password" id="senha" name="senha" placeholder="Senha"/>
-    <br/><br/>
-    <input type="submit" value="Entrar"/>&nbsp;
-    <input type="reset" value="Resetar"/>
-</form>
+    }
+    ?>
+</div>
+<script type="text/javascript" src="js/jquery-3.5.1.slim.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
