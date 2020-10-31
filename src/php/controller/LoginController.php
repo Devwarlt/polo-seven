dc9810954d813973adcbc3e58888268da5bf675f
Devwarlt <devwarlt@loesoft.org>
2020-10-31 00:40:16 -0200

    Update LoginController.php

--- src/php/controller/LoginController.php
+++ src/php/controller/LoginController.php
@@ -8,8 +8,10 @@
 
 namespace php\controller;
 
-use php\dao\LoginDAO as login;
+use php\dao\LoginDAO;
+use php\dao\UsuarioDAO;
 use php\model\LoginModel;
+use php\model\UsuarioModel;
 
 define("LOGIN_ID", "login-id");
 define("LOGIN_NAME", "login-name");
@@ -17,13 +19,8 @@ define("LOGIN_PASSWORD", "login-password");
 
 final class LoginController
 {
-    public const SECOND = 1;
-    public const MINUTE_TO_SECONDS = 60;
-    public const HOUR_TO_SECONDS = 3600;
-    public const DAY_TO_SECONDS = 86400;
-
-    private static $name_regex_pattern = "/^[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]{3,128}$/";
-    private static $password_regex_pattern = "/^[a-zA-Z0-9'\"!@#$%¨&*()_+¹²³£¢¬§=-]{3,128}$/";
+    public static $NAME_REGEX_PATTERN = "/^[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]{3,128}$/";
+    public static $PASSWORD_REGEX_PATTERN = "/^[a-zA-Z0-9'\"!@#$%¨&*()_+¹²³£¢¬§=-]{3,128}$/";
 
     private static $singleton;
 
@@ -45,18 +42,19 @@ final class LoginController
             "err" => null
         );
 
-        if (!preg_match(self::$name_regex_pattern, $nome)) {
+        if (!preg_match(self::$NAME_REGEX_PATTERN, $nome)) {
             $result["err"] = "Nome inválido!";
             return $result;
         }
 
-        if (!preg_match(self::$password_regex_pattern, $senha)) {
+        if (!preg_match(self::$PASSWORD_REGEX_PATTERN, $senha)) {
             $result["err"] = "Senha inválida!";
             return $result;
         }
 
-        $dao = login::getSingleton();
-        if (($result["login"] = $dao->consultarLogin($nome, $senha)) === null) {
+        $login = new LoginModel(-1, $nome, $senha, -1);
+        $dao = LoginDAO::getSingleton();
+        if (($result["login"] = $dao->consultarLogin($login)) === null) {
             $result["err"] = "Credenciais não autenticadas!";
             return $result;
         }
@@ -64,7 +62,47 @@ final class LoginController
         return $result;
     }
 
-    public function criarSessaoLogin(LoginModel $login): void
+    public function criarCredenciais(string $nome, string $senha): array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(self::$NAME_REGEX_PATTERN, $nome)) {
+            $result["err"] = "Nome inválido!";
+            return $result;
+        }
+
+        if (!preg_match(self::$PASSWORD_REGEX_PATTERN, $senha)) {
+            $result["err"] = "Senha inválida!";
+            return $result;
+        }
+
+        $login = new LoginModel(-1, $nome, $senha, -1);
+        $loginDao = LoginDAO::getSingleton();
+        if (($loginDao->consultarLogin($login) !== null)) {
+            $result["err"] = "Já existe um cadastro com esses dados, utilize outra senha se preferir manter o mesmo nome.";
+            return $result;
+        }
+
+        $usrDao = UsuarioDAO::getSingleton();
+        $usr = new UsuarioModel(-1, UsuarioModel::GERENTE);
+        if (($id = $usrDao->criarUsuario($usr)) === null) {
+            $result["err"] = "Não foi possível criar o usuário!";
+            return $result;
+        }
+
+        if (!$loginDao->criarLogin($login, $id)) {
+            $result["err"] = "Não foi possível criar o login!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function criarSessao(LoginModel $login): void
     {
         session_start();
 
@@ -73,7 +111,7 @@ final class LoginController
         $_SESSION[LOGIN_PASSWORD] = $login->getSenha();
     }
 
-    public function terminarSessaoLogin(): void
+    public function terminarSessao(): void
     {
         session_start();
 
@@ -82,7 +120,7 @@ final class LoginController
         unset($_SESSION[LOGIN_PASSWORD]);
     }
 
-    public function verificarSessaoLogin(): bool
+    public function verificarSessao(): bool
     {
         return array_key_exists(LOGIN_ID, $_SESSION) && array_key_exists(LOGIN_NAME, $_SESSION) && array_key_exists(LOGIN_PASSWORD, $_SESSION);
     }
