741d6bc0d458b93d3d52a9037e2fac9f7d34b075
Devwarlt <devwarlt@loesoft.org>
2020-10-31 13:20:13 -0200

    Update DashboardController.php

--- src/php/controller/DashboardController.php
+++ src/php/controller/DashboardController.php
@@ -104,7 +104,7 @@ final class DashboardController
         }
 
         $loginDao = LoginDAO::getSingleton();
-        if (!$loginDao->alterarPagamento($login)) {
+        if (!$loginDao->alterarLogin($login)) {
             $result["err"] = "Não foi possível alterar as credenciais do gerente!";
             return $result;
         }
@@ -112,6 +112,151 @@ final class DashboardController
         $usrDao = UsuarioDAO::getSingleton();
         if (!$usrDao->alterarUsuario($usr)) {
             $result["err"] = "Não foi possível alterar o nível de acesso do gerente!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function removerGerente(int $id, int $id_usuario): ?array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $id) || !preg_match(self::$id_regex_pattern, $id_usuario)) {
+            $result["err"] = "Índice inválido! Somente valor positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $usrDao = UsuarioDAO::getSingleton();
+        if (!$usrDao->removerUsuario($id_usuario)) {
+            $result["err"] = "Não foi possível remover o gerente!";
+            return $result;
+        }
+
+        $loginDao = LoginDAO::getSingleton();
+        if (!$loginDao->removerLogin($id)) {
+            $result["err"] = "Não foi possível remover as credenciais do gerente!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function consultarVendedorId(int $id): ?array
+    {
+        $result = array(
+            "status" => false,
+            "usr" => null,
+            "login" => null,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $id)) {
+            $result["err"] = "Índice inválido! Somente valor positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $loginDao = LoginDAO::getSingleton();
+        if (($login = $loginDao->consultarLoginId($id)) === null) {
+            $result["err"] = "Nenhum cadastro encontrado!";
+            return $result;
+        }
+
+        $usrDao = UsuarioDAO::getSingleton();
+        if (($usr = $usrDao->consultarUsuarioPorNivel($id, UsuarioModel::VENDEDOR)) === null) {
+            $result["err"] = "Este usuário não é um vendedor!";
+            return $result;
+        }
+
+        $result["usr"] = $usr;
+        $result["login"] = $login;
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function consultarVendedores(): ?array
+    {
+        $result = array(
+            "status" => false,
+            "entries" => null,
+            "err" => null
+        );
+        $usrDao = UsuarioDAO::getSingleton();
+        if (($collection = $usrDao->consultarUsuariosPorNivel(UsuarioModel::VENDEDOR)) === null) {
+            $result["err"] = "Não existem registros de vendedores!";
+            return $result;
+        }
+
+        $result["entries"] = $collection;
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function alterarVendedor(LoginModel $login, UsuarioModel $usr): ?array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(LoginController::$NAME_REGEX_PATTERN, $login->getNome())) {
+            $result["err"] = "Nome inválido!";
+            return $result;
+        }
+
+        if (!preg_match(LoginController::$PASSWORD_REGEX_PATTERN, $login->getSenha())) {
+            $result["err"] = "Senha inválida!";
+            return $result;
+        }
+
+        $nivel = $usr->getNivel();
+        if ($nivel < UsuarioModel::GERENTE || $nivel > UsuarioModel::VENDEDOR) {
+            $result["err"] = "Nível de Acesso inválido!";
+            return $result;
+        }
+
+        $loginDao = LoginDAO::getSingleton();
+        if (!$loginDao->alterarLogin($login)) {
+            $result["err"] = "Não foi possível alterar as credenciais do vendedor!";
+            return $result;
+        }
+
+        $usrDao = UsuarioDAO::getSingleton();
+        if (!$usrDao->alterarUsuario($usr)) {
+            $result["err"] = "Não foi possível alterar o nível de acesso do vendedor!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function removerVendedor(int $id, int $id_usuario): ?array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $id) || !preg_match(self::$id_regex_pattern, $id_usuario)) {
+            $result["err"] = "Índice inválido! Somente valor positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $usrDao = UsuarioDAO::getSingleton();
+        if (!$usrDao->removerUsuario($id_usuario)) {
+            $result["err"] = "Não foi possível remover o vendedor!";
+            return $result;
+        }
+
+        $loginDao = LoginDAO::getSingleton();
+        if (!$loginDao->removerLogin($id)) {
+            $result["err"] = "Não foi possível remover as credenciais do vendedor!";
             return $result;
         }
 
