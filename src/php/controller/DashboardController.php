4f56299136be0383b43bff6c0d5560eca45d8377
Devwarlt <devwarlt@loesoft.org>
2020-11-01 04:14:38 -0200

    Update DashboardController.php

--- src/php/controller/DashboardController.php
+++ src/php/controller/DashboardController.php
@@ -9,9 +9,15 @@
 namespace php\controller;
 
 use php\dao\LoginDAO;
+use php\dao\ProdutoDAO;
+use php\dao\PagamentoDAO;
 use php\dao\UsuarioDAO;
+use php\dao\VendaDAO;
 use php\model\LoginModel;
+use php\model\ProdutoModel;
+use php\model\PagamentoModel;
 use php\model\UsuarioModel;
+use php\model\VendaModel;
 
 final class DashboardController
 {
@@ -40,7 +46,7 @@ final class DashboardController
         );
 
         if (!preg_match(self::$id_regex_pattern, $id)) {
-            $result["err"] = "Índice inválido! Somente valor positivos e não nulos serão aceitos!";
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
             return $result;
         }
 
@@ -87,6 +93,11 @@ final class DashboardController
             "err" => null
         );
 
+        if (!preg_match(self::$id_regex_pattern, $login->getId()) || !preg_match(self::$id_regex_pattern, $usr->getId())) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
         if (!preg_match(LoginController::$NAME_REGEX_PATTERN, $login->getNome())) {
             $result["err"] = "Nome inválido!";
             return $result;
@@ -127,7 +138,7 @@ final class DashboardController
         );
 
         if (!preg_match(self::$id_regex_pattern, $id) || !preg_match(self::$id_regex_pattern, $id_usuario)) {
-            $result["err"] = "Índice inválido! Somente valor positivos e não nulos serão aceitos!";
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
             return $result;
         }
 
@@ -157,7 +168,7 @@ final class DashboardController
         );
 
         if (!preg_match(self::$id_regex_pattern, $id)) {
-            $result["err"] = "Índice inválido! Somente valor positivos e não nulos serão aceitos!";
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulo serão aceitos!";
             return $result;
         }
 
@@ -204,6 +215,11 @@ final class DashboardController
             "err" => null
         );
 
+        if (!preg_match(self::$id_regex_pattern, $login->getId()) || !preg_match(self::$id_regex_pattern, $usr->getId())) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
         if (!preg_match(LoginController::$NAME_REGEX_PATTERN, $login->getNome())) {
             $result["err"] = "Nome inválido!";
             return $result;
@@ -244,7 +260,7 @@ final class DashboardController
         );
 
         if (!preg_match(self::$id_regex_pattern, $id) || !preg_match(self::$id_regex_pattern, $id_usuario)) {
-            $result["err"] = "Índice inválido! Somente valor positivos e não nulos serão aceitos!";
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
             return $result;
         }
 
@@ -257,6 +273,345 @@ final class DashboardController
         $loginDao = LoginDAO::getSingleton();
         if (!$loginDao->removerLogin($id)) {
             $result["err"] = "Não foi possível remover as credenciais do vendedor!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function criarProduto(ProdutoModel $produto): array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(LoginController::$PRODUCT_NAME_REGEX_PATTERN, $produto->getNome())) {
+            $result["err"] = "Nome inválido!";
+            return $result;
+        }
+
+        $produtoDao = ProdutoDAO::getSingleton();
+        if ($produtoDao->consultarProdutoNome($produto->getNome()) !== null) {
+            $result["err"] = "Já existe um registro com o mesmo nome, utilize outro para continuar.";
+            return $result;
+        }
+
+        if (!$produtoDao->criarProduto($produto)) {
+            $result["err"] = "Não foi possível criar o produto!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function consultarProdutoId(int $id): ?array
+    {
+        $result = array(
+            "status" => false,
+            "produto" => null,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $id)) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $produtoDao = ProdutoDAO::getSingleton();
+        if (($produto = $produtoDao->consultarProduto($id)) === null) {
+            $result["err"] = "Nenhum registro encontrado!";
+            return $result;
+        }
+
+        $result["produto"] = $produto;
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function consultarProdutos(): ?array
+    {
+        $result = array(
+            "status" => false,
+            "entries" => null,
+            "err" => null
+        );
+        $produtoDao = ProdutoDAO::getSingleton();
+        if (($collection = $produtoDao->consultarProdutos()) === null) {
+            $result["err"] = "Não existem registros de produtos!";
+            return $result;
+        }
+
+        $result["entries"] = $collection;
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function alterarProduto(ProdutoModel $produto): ?array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $produto->getId())) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        if (!preg_match(LoginController::$PRODUCT_NAME_REGEX_PATTERN, $produto->getNome())) {
+            $result["err"] = "Nome inválido!";
+            return $result;
+        }
+
+        $produtoDao = ProdutoDAO::getSingleton();
+        if (!$produtoDao->alterarProduto($produto)) {
+            $result["err"] = "Não foi possível alterar os dados do produto!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function removerProduto(int $id): ?array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $id)) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $produtoDao = ProdutoDAO::getSingleton();
+        if (!$produtoDao->removerProduto($id)) {
+            $result["err"] = "Não foi possível remover o produto!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function criarPagamento(PagamentoModel $pagamento): array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(LoginController::$NAME_REGEX_PATTERN, $pagamento->getNome())) {
+            $result["err"] = "Nome inválido!";
+            return $result;
+        }
+
+        $pagamentoDao = PagamentoDAO::getSingleton();
+        if ($pagamentoDao->consultarPagamentoNome($pagamento->getNome()) !== null) {
+            $result["err"] = "Já existe um registro com o mesmo nome, utilize outro para continuar.";
+            return $result;
+        }
+
+        if (!$pagamentoDao->criarPagamento($pagamento)) {
+            $result["err"] = "Não foi possível criar o pagamento!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function consultarPagamentoId(int $id): ?array
+    {
+        $result = array(
+            "status" => false,
+            "pagamento" => null,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $id)) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $pagamentoDao = PagamentoDAO::getSingleton();
+        if (($pagamento = $pagamentoDao->consultarPagamento($id)) === null) {
+            $result["err"] = "Nenhum registro encontrado!";
+            return $result;
+        }
+
+        $result["pagamento"] = $pagamento;
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function consultarPagamentos(): ?array
+    {
+        $result = array(
+            "status" => false,
+            "entries" => null,
+            "err" => null
+        );
+        $pagamentoDao = PagamentoDAO::getSingleton();
+        if (($collection = $pagamentoDao->consultarPagamentos()) === null) {
+            $result["err"] = "Não existem registros de pagamentos!";
+            return $result;
+        }
+
+        $result["entries"] = $collection;
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function alterarPagamento(PagamentoModel $pagamento): ?array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $pagamento->getId())) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        if (!preg_match(LoginController::$NAME_REGEX_PATTERN, $pagamento->getNome())) {
+            $result["err"] = "Nome inválido!";
+            return $result;
+        }
+
+        $pagamentoDao = PagamentoDAO::getSingleton();
+        if (!$pagamentoDao->alterarPagamento($pagamento)) {
+            $result["err"] = "Não foi possível alterar os dados do pagamento!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function removerPagamento(int $id): ?array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $id)) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $pagamentoDao = PagamentoDAO::getSingleton();
+        if (!$pagamentoDao->removerPagamento($id)) {
+            $result["err"] = "Não foi possível remover o pagamento!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function criarVenda(VendaModel $venda): array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        $vendaDao = VendaDAO::getSingleton();
+        if (!$vendaDao->criarVenda($venda)) {
+            $result["err"] = "Não foi possível criar a venda!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function consultarVendaId(int $id): ?array
+    {
+        $result = array(
+            "status" => false,
+            "venda" => null,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $id)) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $vendaDao = VendaDAO::getSingleton();
+        if (($venda = $vendaDao->consultarVenda($id)) === null) {
+            $result["err"] = "Nenhum registro encontrado!";
+            return $result;
+        }
+
+        $result["venda"] = $venda;
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function consultarVendas(): ?array
+    {
+        $result = array(
+            "status" => false,
+            "entries" => null,
+            "err" => null
+        );
+        $vendaDao = VendaDAO::getSingleton();
+        if (($collection = $vendaDao->consultarVendas()) === null) {
+            $result["err"] = "Não existem registros de vendas!";
+            return $result;
+        }
+
+        $result["entries"] = $collection;
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function alterarVenda(VendaModel $venda): ?array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $venda->getId())) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $vendaDao = VendaDAO::getSingleton();
+        if (!$vendaDao->alterarVenda($venda)) {
+            $result["err"] = "Não foi possível alterar os dados da venda!";
+            return $result;
+        }
+
+        $result["status"] = true;
+        return $result;
+    }
+
+    public function removerVenda(int $id): ?array
+    {
+        $result = array(
+            "status" => false,
+            "err" => null
+        );
+
+        if (!preg_match(self::$id_regex_pattern, $id)) {
+            $result["err"] = "Índice inválido! Somente valores positivos e não nulos serão aceitos!";
+            return $result;
+        }
+
+        $vendaDao = VendaDAO::getSingleton();
+        if (!$vendaDao->removerVenda($id)) {
+            $result["err"] = "Não foi possível remover a venda!";
             return $result;
         }
 
